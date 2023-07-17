a1ff2c11795fc66dd4e8a791ed85de4a22f22ed2
lawanya025 <135047184+lawanya025@users.noreply.github.com>
2023-07-17 21:50:59 +0530

    Delete RedFlag directory

--- RedFlag/constants.py
+++
@@ -1,5 +0,0 @@
-"""
-Purpose:
-    Store Constants and Queries
-"""
-lang = 'en'
--- RedFlag/serve.py
+++
@@ -1,4 +0,0 @@
-import waitress
-import Back_Pain_App
-
-waitress.serve(Back_Pain_App.app, host='0.0.0.0', port=40000)
